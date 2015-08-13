52d6fc6d98e070b1b38e409906f2888e92a4ca8b
He Qiyong <kiwony@163.com>
2015-08-13 11:25:04 +0800

    fix big

--- algorithms/2_add_two_numbers/add-two-numbers.c
+++ algorithms/2_add_two_numbers/add-two-numbers.c
@@ -11,10 +11,11 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
     struct ListNode * sumHead=NULL ;
     struct ListNode * sumEnd=NULL ;
     struct ListNode * newNode ;
-    while(l1 != NULL || l2 != NULL || cin!=0){
-        firstVal = (l1 == NULL) ? 0 : l1->val;
-        secondVal = (l2 == NULL) ? 0 : l2->val;
+    while(l1 || l2 || cin!=0){
+        firstVal = l1 ? l1->val : 0;
+        secondVal = l2 ?  l2->val : 0;
         firstVal = firstVal + secondVal + cin;
+        cin = firstVal / 10;  //cin for next
         firstVal = firstVal % 10;
         newNode = malloc(sizeof(struct ListNode));
         newNode->val = firstVal;
@@ -26,21 +27,18 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
             sumEnd = sumEnd->next;
         }
         // for next
-        cin = firstVal / 10;
-        if (l1 != NULL) l1 = l1->next;
-        if (l2 != NULL) l2 = l2->next;
+        if (l1) l1 = l1->next;
+        if (l2) l2 = l2->next;
     }
     return sumHead;
 }
 
 int main(){
     struct ListNode *hehe;
-    struct ListNode a;
-    struct ListNode b;
-    a.val = 1;
-    a.next = NULL;
-    b.val = 4;
-    b.next=NULL;
+    struct ListNode a = {9, NULL};
+    struct ListNode b = {4, NULL};
+    struct ListNode c = {1, NULL};
+    a.next = &c;
     hehe = addTwoNumbers(&a, &b);
-    printf("%d\n", hehe->val);
+    printf("%d,%d\n", hehe->val, hehe->next->val);
 }
