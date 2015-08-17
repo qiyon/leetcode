7b2b796ffad8bf03f9bbb1f85ce2844b1a42cc50
He Qiyong <kiwony@163.com>
2015-08-17 17:45:36 +0800

    for problem 4

---
+++ algorithms/4_Median-of-Two-Sorted-Arrays/py_solution.py
@@ -0,0 +1,15 @@
+## leetcode submission begin ---------------------
+class Solution:
+    # @param {integer[]} nums1
+    # @param {integer[]} nums2
+    # @return {float}
+    def findMedianSortedArrays(self, nums1, nums2):
+                       
+
+##leetcode submission end ---------------------
+
+if __name__ == "__main__":
+    slObj = Solution()
+    print slObj.findMedianSortedArrays([1,2,5,7,9,12,16,18,23], [1,2,3,4,5,6,6.5,8,9])
+    ##print slObj.findMedianSortedArrays([1,2,3,4,5,6,6,7],[])
+    ##print slObj.findMedianSortedArrays([1,2,3,4,5,6,6,7],[100])
