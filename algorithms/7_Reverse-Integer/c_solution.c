0dd3571a5ddc3c456318c81015904b1629146bec
He Qiyong <kiwony@163.com>
2015-08-23 12:54:11 +0800

    bad solution of c

--- algorithms/7_Reverse-Integer/c_solution.c
+++ algorithms/7_Reverse-Integer/c_solution.c
@@ -4,7 +4,24 @@
 //leetcode submission begin--------------------
 int reverse(int x) 
 {
-       return 0; 
+    long long isNeg = 0;
+    long long retNum = 0;
+    long long longx = x;
+    //int preRetNum = 0;
+    if (x>-10 && x<10) return x;
+    if (longx < 0){
+        longx  = -longx;
+        isNeg = 1;
+    }
+    while(longx){
+        retNum = (retNum * 10) + (longx % 10);
+        //printf("nowNum:%ld now_x:%ld\n---------------\n", retNum, longx);
+        if ( retNum > 2147483647 + isNeg ){
+            return 0; 
+        }
+        longx = longx / 10;
+    }
+    return (isNeg) ? -retNum : retNum;
 }
 //leetcode submission end--------------------
 
@@ -12,5 +29,4 @@ int main(int argc, char * argv[])
 {
     int x = atoi(argv[1]);
     printf("%d\n", reverse(x));
-
 }
