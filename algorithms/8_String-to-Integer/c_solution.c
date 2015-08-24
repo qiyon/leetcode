e2dc7f85b0a8d6480665300f639a54facbd2f581
He Qiyong <kiwony@163.com>
2015-08-24 13:38:40 +0800

    c solution of No.8

--- algorithms/8_String-to-Integer/c_solution.c
+++ algorithms/8_String-to-Integer/c_solution.c
@@ -2,7 +2,27 @@
 
 //leetcode submission bgein -----------------------------
 int myAtoi(char* str) {
-    return 0; 
+    int slen = strlen(str);
+    int i = 0;
+    int isNeg = 0;
+    int outPutNum = 0;
+    int notSignYet = 1;
+    while( (str[i] == ' ' || str[i] == '-' || str[i] == '+') && notSignYet ){
+        if (str[i] == '-') isNeg = 1;
+        if (str[i] == '+' || str[i] == '-') notSignYet = 0;
+        i++;
+        //printf("%c\n---------\n", str[i]);
+    }
+    while( str[i] >= '0' && str[i]<='9' && i<slen) {
+        if (isNeg){
+            if (outPutNum > INT_MAX / 10 || (outPutNum == INT_MAX /10 && str[i] >= '8')) return INT_MIN;
+        }else{
+            if (outPutNum > INT_MAX / 10 || (outPutNum == INT_MAX /10 && str[i] >= '7')) return INT_MAX;
+        }
+        outPutNum = outPutNum * 10 + (str[i] - '0');
+        i++;
+    }
+    return (isNeg) ? -outPutNum : outPutNum;
 }
 //leetcode submission end -----------------------------
 
