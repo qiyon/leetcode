d6cce787864c87ead01cb0e77737c02c24500fae
He Qiyong <kiwony@163.com>
2015-08-27 22:55:07 +0800

    4ms solution ,goodgit add c_solution.cgit add c_solution.c

--- algorithms/010_Regular-Expression-Matching/c_solution.c
+++ algorithms/010_Regular-Expression-Matching/c_solution.c
@@ -1,5 +1,16 @@
 #include "../c_leetcode.h"
 
+
+void selfPrintStatus(int *s, int length)
+{
+    int i;
+    for(i=0; i< length; i++)
+    {
+        printf("%d,", s[i]);
+    }
+    printf("\n--------------------\n");
+}
+
 //leetcode submission begin ---------------------
 void initStatus(int *statusArr, int length)
 {
@@ -11,44 +22,31 @@ void initStatus(int *statusArr, int length)
 }
 void insertVal(int * statusArr, int length, int insertVal)
 {
-    int i = 0, relaceIndex = -1;
-    if (insertVal >= length) return;
+    int i = 0;
+    if (length <= insertVal) return;
     while(i < length){
-        if (relaceIndex == -1 && statusArr[i] == -1) relaceIndex = i;
         if (statusArr[i] == insertVal) return;
+        if (statusArr[i] == -1 ){
+            statusArr[i] = insertVal;
+            return;
+        }
         i++;
     }
-    printf("---\nset status[%d] = %d \n---\n", relaceIndex, insertVal);
-    statusArr[relaceIndex] = insertVal;
-}
-
-void swapStatus(int * s1, int * s2)
-{
-    int * tmp;
-    tmp = s1;
-    s1 = s2;
-    s2 = tmp;
-}
-void selfPrintStatus(int *s, int length)
-{
-    int i;
-    for(i=0; i< length; i++)
-    {
-        printf("%d,", s[i]);
-    }
-    printf("\n--------------------\n");
 }
 
 bool isMatch(char* s, char* p) 
 {
     int slen = strlen(s), plen = strlen(p); //length of string and pattern
     int * ruleIndex, * ruleRepeat;
-    int * nowStatus, * preStatus;
-    int i = 0, j = 0, countOfRule = 0;
+    int * nowStatus, * preStatus, * swapStatusTmp;
+    int i = 0, j = 0, countOfRule = 0, tmpMoveIndex;
     int valIsMatch = 0;
     int checkStatusTmp;
 
-    ruleIndex = (int *)malloc(sizeof(int) * plen );
+    if (plen == 0) {
+        return (slen == 0) ? 1 : 0;
+    }
+    ruleIndex = (int *)malloc(sizeof(int) * plen);
     ruleRepeat = (int *)malloc(sizeof(int) * plen);
     while( i < plen ){
         ruleIndex[countOfRule] = i;
@@ -61,29 +59,40 @@ bool isMatch(char* s, char* p)
         }
         countOfRule++;
     }
+    //printf("count of rule:%d\n---\n",countOfRule);
     nowStatus = (int *)malloc(sizeof(int)*(countOfRule + 1));
     preStatus = (int *)malloc(sizeof(int)*(countOfRule + 1));
     initStatus(nowStatus, countOfRule + 1);
     initStatus(preStatus, countOfRule + 1);
     preStatus[0] = 0;
     for(i = 0 ; i < slen; i++){
-        selfPrintStatus(preStatus,countOfRule+1);
+        //printf("--------one char ------------------------\n");
+        //selfPrintStatus(preStatus,countOfRule+1);
         for(j = 0; j < countOfRule + 1; j++){
-            selfPrintStatus(nowStatus,countOfRule+1);
             if (preStatus[j] == -1) continue;
-            printf("---\npresataus[j]: %d \n---\n",preStatus[j]  );
-            if (s[i] == p[ruleIndex[preStatus[j]]] || p[ruleIndex[preStatus[j]]] == '.'){
-                insertVal(nowStatus, countOfRule+1, preStatus[j]+1);
+            //printf("---\npresataus[j]: %d ,s[i]=%c,ruleChar=%c \n---\n",preStatus[j] , s[i], p[ruleIndex[preStatus[j]]]  );
+            tmpMoveIndex = preStatus[j];
+            while(tmpMoveIndex < countOfRule && ruleRepeat[tmpMoveIndex]){
+                if (s[i] == p[ruleIndex[tmpMoveIndex]] || p[ruleIndex[tmpMoveIndex]] == '.'){
+                    insertVal(nowStatus, countOfRule+1, tmpMoveIndex);
+                }
+                tmpMoveIndex++;
             }
-            if (ruleRepeat[preStatus[j]])
-                insertVal(nowStatus, countOfRule+1, preStatus[j]);
+            if ( tmpMoveIndex < countOfRule && ( s[i] == p[ruleIndex[tmpMoveIndex]] || p[ruleIndex[tmpMoveIndex]] == '.')){
+                insertVal(nowStatus, countOfRule+1, tmpMoveIndex + 1);
+            }
+            //selfPrintStatus(nowStatus,countOfRule+1);
+            //printf("i:%d ,j:%d ,countOfRule:%d , tmpMoveIndex:%d \n--------------\n", i, j , countOfRule, tmpMoveIndex);
         }
-        swapStatus(nowStatus, preStatus);
+        swapStatusTmp = preStatus;
+        preStatus = nowStatus;
+        nowStatus =swapStatusTmp;
         initStatus(nowStatus, countOfRule+1);
     }
     for(i = 0; i < countOfRule + 1; i++){
         checkStatusTmp = preStatus[i]; 
-        while( checkStatusTmp < countOfRule && ruleRepeat[checkStatusTmp+1]) checkStatusTmp++;
+        if (checkStatusTmp == -1) continue;
+        while( checkStatusTmp < countOfRule && ruleRepeat[checkStatusTmp]) checkStatusTmp++;
         if (checkStatusTmp == countOfRule){
             valIsMatch = 1;
             break;
