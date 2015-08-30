4e57b0a2256b1ac6b51c84e7cc337164427bc234
He Qiyong <kiwony@163.com>
2015-08-30 16:33:31 +0800

    sl for No.12

--- algorithms/012_Integer-to-Roman/c_solution.c
+++ algorithms/012_Integer-to-Roman/c_solution.c
@@ -3,8 +3,55 @@
 //leetcode submission begin ------------------
 char* intToRoman(int num)
 {
-    char * romanStr = (char *)malloc(sizeof(char)*10);
-    romanStr = "Example";
+    char oneArr[]  = {'I','X','C','M'};
+    char fiveArr[] = {'V','L','D'};
+    int i = 0, j=0, romanIndex = 0, modValue;
+    char tmpRoman[4][5];
+    char * romanStr = (char *)malloc(sizeof(char)*15);
+
+    for(i = 0;i<4;i++){
+        for(j=0;j<5;j++){
+            tmpRoman[i][j] = '\0';
+        }
+    }
+    if (num == 0 || num > 3999) {
+        romanStr = "";
+        return romanStr;
+    }
+    i = 0;
+    while (num) {
+        modValue = num % 10;
+        romanIndex = 0;
+        if (modValue == 4 || modValue == 9){
+            tmpRoman[i][romanIndex] = oneArr[i];
+            romanIndex++;
+            tmpRoman[i][romanIndex] = (modValue == 4) ? fiveArr[i] : oneArr[i+1] ;
+            romanIndex++;
+            modValue = 0;
+        }
+        if (modValue >= 5 && modValue < 9){
+            tmpRoman[i][romanIndex] = fiveArr[i];
+            romanIndex++;
+            modValue = modValue - 5;
+        }
+        while (modValue) {
+            tmpRoman[i][romanIndex] = oneArr[i];
+            romanIndex++;
+            modValue--;
+        }
+        num = num / 10;
+        i++;
+    }
+    romanIndex = 0;
+    for (i = 3 ; i>=0; i--){
+        for(j = 0;j < 5; j++){
+            if (tmpRoman[i][j] != '\0'){
+                romanStr[romanIndex] = tmpRoman[i][j]; 
+                romanIndex++;
+            }
+        }
+    }
+    romanStr[romanIndex] = '\0';
     return romanStr;
 }
 //leetcode submission end ------------------
