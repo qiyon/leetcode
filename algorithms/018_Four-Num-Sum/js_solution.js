2bd139ca94a195159b0b6f87377201f0bbb1ce18
qiyon <heqiyon@gmail.com>
2016-09-26 23:19:18 +0800

    init 018

---
+++ algorithms/018_Four-Num-Sum/js_solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function(nums, target) {
+    var out = [];
+    out.push([-1, 0, 0, 1]);
+    return out;
+};
+
+console.log(fourSum([1, 0, -1, 0, -2, 2], 0));
+
