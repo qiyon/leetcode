77a5bc04c9296342c7bbdfb6aee2298624e0385f
qiyon <heqiyon@gmail.com>
2016-10-14 23:10:22 +0800

    018 solution

--- algorithms/018_Four-Num-Sum/js_solution.js
+++ algorithms/018_Four-Num-Sum/js_solution.js
@@ -5,9 +5,38 @@
  */
 var fourSum = function(nums, target) {
     var out = [];
-    out.push([-1, 0, 0, 1]);
+    var len = nums.length;
+    var i, j, left, right, tmp_target;
+    nums = nums.sort(function (a, b) {
+      return a - b;
+    });
+    for (i = 0; i < len - 3; i++) {
+      if (i > 0 && nums[i] == nums[i - 1]){
+        continue;
+      }
+      for (j = i + 1; j < len -2; j++) {
+        if (j > i + 1 && nums[j] == nums[j - 1]){
+          continue;
+        }
+        left = j + 1;
+        right = len - 1;
+        while (left < right) {
+          tmp_target = nums[i] + nums[j] + nums[left] + nums[right];
+          if (tmp_target < target) {
+            left++;
+          } else if (tmp_target > target){
+            right--;
+          }else{
+            out.push([nums[i], nums[j], nums[left], nums[right]]);
+            while (left < right && nums[left] == nums[left + 1]){
+              left++;
+            }
+            left++;
+          }
+        }
+      }
+    }
     return out;
 };
 
-console.log(fourSum([1, 0, -1, 0, -2, 2], 0));
-
+console.log(fourSum([-3, -2, -1, 0, 0, 1, 2, 3], 0));
